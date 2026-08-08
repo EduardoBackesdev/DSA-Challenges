95b528bac7f470ee02f65f9450e2851833aaff0c
Eduardo Backes <eduardo.programacao@hotmail.com>
2026-08-08 03:19:17 -0300

    fix challenge 2

--- DSA/2/remove-element.go
+++ DSA/2/remove-element.go
@@ -3,18 +3,20 @@ package main
 func removeElement(nums []int, val int) int {
 
 	p1 := 0
-	p2 := 1
-	len := len(nums) - 1
+	p2 := len(nums) - 1
 
-	for p2 <= len {
+	for p1 <= p2 {
 		if nums[p1] == val {
 			a := nums[p1]
 			b := nums[p2]
 			nums[p1] = b
 			nums[p2] = a
-			p2++
+			p2--
+		} else {
+			p1++
 		}
-
 	}
+
+	return p1
 
 }
